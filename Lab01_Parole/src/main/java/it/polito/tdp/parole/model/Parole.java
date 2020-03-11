dc8f3071a7e777b7f153a9a973adfb0df274318a
gabrielecarru <gabrielecarru@localhost>
2020-03-11 10:27:17 +0100

    Primo Commit, definizione metodi classe 'parole'

--- Lab01_Parole/src/main/java/it/polito/tdp/parole/model/Parole.java
+++ Lab01_Parole/src/main/java/it/polito/tdp/parole/model/Parole.java
@@ -1,24 +1,45 @@
 package it.polito.tdp.parole.model;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class Parole {
+public class Parole implements Comparator<String> {
+	
+		// Struttura dati utilizzata
+		private List<String> elenco = new ArrayList<String>();
+//		private List<String> elencoOrdinato = new ArrayList<String>();
 		
 	public Parole() {
 		//TODO
+		
 	}
 	
 	public void addParola(String p) {
-		//TODO
+		elenco.add(p);
 	}
 	
 	public List<String> getElenco() {
-		//TODO
-		return null;
+		
+//		elencoOrdinato = Collections.sort(elenco);
+//		elencoOrdinato=elenco.stream().sorted().collect(Collectors.toList());
+//		List<String> elencoOrdinato = elenco.stream()
+//										.sorted()
+//										.collect(Collectors.toList());
+		List<String> eO = elenco.stream().sorted().collect(Collectors.toCollection(ArrayList::new));
+										
+		return eO;
 	}
 	
 	public void reset() {
-		// TODO
+		elenco.clear();
+	}
+
+	@Override
+	public int compare(String o1, String o2) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 }
